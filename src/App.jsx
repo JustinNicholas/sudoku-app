c90490dec5faccd1670168ec1de901600896b5cf
Justin Nicholas <nicholasj964@yahoo.com>
2022-12-29 10:13:08 -0600

    starting files and frame of sudoku app

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+      </header>
+
+      <main>
+        <div className='whole-box'>
+          <div className='box-1 boxes'>
+
+          </div>
+          <div className='box-2 boxes'>
+
+          </div>
+          <div className='box-3 boxes'>
+
+          </div>
+          <div className='box-4 boxes'>
+
+          </div>
+          <div className='box-5 boxes'>
+
+          </div>
+          <div className='box-6 boxes'>
+
+          </div>
+          <div className='box-7 boxes'>
+
+          </div>
+          <div className='box-8 boxes'>
+
+          </div>
+          <div className='box-9 boxes'>
+
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
