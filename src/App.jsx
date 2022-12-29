c10783ccb1c787c7145c61eac442de9aa3a4fa43
Justin Nicholas <nicholasj964@yahoo.com>
2022-12-29 10:25:32 -0600

    grid complete

--- src/App.jsx
+++ src/App.jsx
@@ -10,31 +10,265 @@ function App() {
       <main>
         <div className='whole-box'>
           <div className='box-1 boxes'>
+            <div className='block-1 blocks'>
 
+            </div>
+            <div className='block-2 blocks'>
+
+            </div>
+            <div className='block-3 blocks'>
+
+            </div>
+            <div className='block-4 blocks'>
+
+            </div>
+            <div className='block-5 blocks'>
+
+            </div>
+            <div className='block-6 blocks'>
+
+            </div>
+            <div className='block-7 blocks'>
+
+            </div>
+            <div className='block-8 blocks'>
+
+            </div>
+            <div className='block-9 blocks'>
+
+            </div>
           </div>
           <div className='box-2 boxes'>
+            <div className='block-1 blocks'>
 
+            </div>
+            <div className='block-2 blocks'>
+
+            </div>
+            <div className='block-3 blocks'>
+
+            </div>
+            <div className='block-4 blocks'>
+
+            </div>
+            <div className='block-5 blocks'>
+
+            </div>
+            <div className='block-6 blocks'>
+
+            </div>
+            <div className='block-7 blocks'>
+
+            </div>
+            <div className='block-8 blocks'>
+
+            </div>
+            <div className='block-9 blocks'>
+
+            </div>
           </div>
           <div className='box-3 boxes'>
+            <div className='block-1 blocks'>
 
+            </div>
+            <div className='block-2 blocks'>
+
+            </div>
+            <div className='block-3 blocks'>
+
+            </div>
+            <div className='block-4 blocks'>
+
+            </div>
+            <div className='block-5 blocks'>
+
+            </div>
+            <div className='block-6 blocks'>
+
+            </div>
+            <div className='block-7 blocks'>
+
+            </div>
+            <div className='block-8 blocks'>
+
+            </div>
+            <div className='block-9 blocks'>
+
+            </div>
           </div>
           <div className='box-4 boxes'>
+            <div className='block-1 blocks'>
 
+            </div>
+            <div className='block-2 blocks'>
+
+            </div>
+            <div className='block-3 blocks'>
+
+            </div>
+            <div className='block-4 blocks'>
+
+            </div>
+            <div className='block-5 blocks'>
+
+            </div>
+            <div className='block-6 blocks'>
+
+            </div>
+            <div className='block-7 blocks'>
+
+            </div>
+            <div className='block-8 blocks'>
+
+            </div>
+            <div className='block-9 blocks'>
+
+            </div>
           </div>
           <div className='box-5 boxes'>
+            <div className='block-1 blocks'>
 
+            </div>
+            <div className='block-2 blocks'>
+
+            </div>
+            <div className='block-3 blocks'>
+
+            </div>
+            <div className='block-4 blocks'>
+
+            </div>
+            <div className='block-5 blocks'>
+
+            </div>
+            <div className='block-6 blocks'>
+
+            </div>
+            <div className='block-7 blocks'>
+
+            </div>
+            <div className='block-8 blocks'>
+
+            </div>
+            <div className='block-9 blocks'>
+
+            </div>
           </div>
           <div className='box-6 boxes'>
+            <div className='block-1 blocks'>
 
+            </div>
+            <div className='block-2 blocks'>
+
+            </div>
+            <div className='block-3 blocks'>
+
+            </div>
+            <div className='block-4 blocks'>
+
+            </div>
+            <div className='block-5 blocks'>
+
+            </div>
+            <div className='block-6 blocks'>
+
+            </div>
+            <div className='block-7 blocks'>
+
+            </div>
+            <div className='block-8 blocks'>
+
+            </div>
+            <div className='block-9 blocks'>
+
+            </div>
           </div>
           <div className='box-7 boxes'>
+            <div className='block-1 blocks'>
 
+            </div>
+            <div className='block-2 blocks'>
+
+            </div>
+            <div className='block-3 blocks'>
+
+            </div>
+            <div className='block-4 blocks'>
+
+            </div>
+            <div className='block-5 blocks'>
+
+            </div>
+            <div className='block-6 blocks'>
+
+            </div>
+            <div className='block-7 blocks'>
+
+            </div>
+            <div className='block-8 blocks'>
+
+            </div>
+            <div className='block-9 blocks'>
+
+            </div>
           </div>
           <div className='box-8 boxes'>
+            <div className='block-1 blocks'>
 
+            </div>
+            <div className='block-2 blocks'>
+
+            </div>
+            <div className='block-3 blocks'>
+
+            </div>
+            <div className='block-4 blocks'>
+
+            </div>
+            <div className='block-5 blocks'>
+
+            </div>
+            <div className='block-6 blocks'>
+
+            </div>
+            <div className='block-7 blocks'>
+
+            </div>
+            <div className='block-8 blocks'>
+
+            </div>
+            <div className='block-9 blocks'>
+
+            </div>
           </div>
           <div className='box-9 boxes'>
+            <div className='block-1 blocks'>
 
+            </div>
+            <div className='block-2 blocks'>
+
+            </div>
+            <div className='block-3 blocks'>
+
+            </div>
+            <div className='block-4 blocks'>
+
+            </div>
+            <div className='block-5 blocks'>
+
+            </div>
+            <div className='block-6 blocks'>
+
+            </div>
+            <div className='block-7 blocks'>
+
+            </div>
+            <div className='block-8 blocks'>
+
+            </div>
+            <div className='block-9 blocks'>
+
+            </div>
           </div>
         </div>
       </main>
